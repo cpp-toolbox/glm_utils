34ec7705b1ce0e39f9a3081e3cd6962a3bf1f83c
ccn <ccn@cuppajoeman.com>
2025-04-01 01:12:52 -0400

    feat: commit various new constants

--- glm_utils.cpp
+++ glm_utils.cpp
@@ -1,7 +1,20 @@
 #include "glm_utils.hpp"
 
+namespace glm_utils {
+
+glm::mat4 identity_matrix(1.0f);
+glm::mat4 zero_matrix(0.0f);
+
+glm::vec3 zero(0, 0, 0);
+glm::vec3 one(1, 1, 1);
+glm::vec3 x(1, 0, 0);
+glm::vec3 y(0, 1, 0);
+glm::vec3 z(0, 0, 1);
+
 /*
  * the direction vector between a and b is b - a, then we want to go halway across this so (b - a) * 0.5
  * and then add this to a to obtain (a + b) * 0.5
  */
 glm::vec3 get_midpoint(const glm::vec3 &a, const glm::vec3 &b) { return (a + b) * 0.5f; }
+
+} // namespace glm_utils
--- glm_utils.hpp
+++ glm_utils.hpp
@@ -3,6 +3,19 @@
 
 #include <glm/glm.hpp>
 
-glm::vec3 get_midpoint(const glm::vec3& a, const glm::vec3& b);
+namespace glm_utils {
+
+extern glm::mat4 identity_matrix;
+extern glm::mat4 zero_matrix;
+
+extern glm::vec3 zero;
+extern glm::vec3 one;
+extern glm::vec3 x;
+extern glm::vec3 y;
+extern glm::vec3 z;
+
+glm::vec3 get_midpoint(const glm::vec3 &a, const glm::vec3 &b);
+
+} // namespace glm_utils
 
 #endif // GLM_UTILS_HPP
